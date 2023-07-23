354a42ae2d9058dc1e444c642f9b64529df3efbd
bgalgamu <kevin.si1396@gmail.com>
2023-07-23 23:04:18 +0200

    changed properties

--- connector/kafka-manager/src/main/java/it/pagopa/selfcare/external_interceptor/connector/kafka_manager/config/KafkaConsumerConfig.java
+++ connector/kafka-manager/src/main/java/it/pagopa/selfcare/external_interceptor/connector/kafka_manager/config/KafkaConsumerConfig.java
@@ -23,9 +23,7 @@ import java.util.Map;
 public class KafkaConsumerConfig {
 
     @Value(value = "${kafka-manager.external-interceptor.clientId}")
-    private String contractsClientId;
-    @Value(value = "${kafka-manager.external-interceptor.clientId}")
-    private String usersClientId;
+    private String clientId;
     @Value(value = "${kafka-manager.external-interceptor.contracts-groupId}")
     private String contractsGroupId;
     @Value(value = "${kafka-manager.external-interceptor.users-groupId}")
@@ -61,7 +59,7 @@ public class KafkaConsumerConfig {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, contractsGroupId);
-        props.put(ConsumerConfig.CLIENT_ID_CONFIG, contractsClientId);
+        props.put(ConsumerConfig.CLIENT_ID_CONFIG, clientId);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffsetReset);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
@@ -80,7 +78,7 @@ public class KafkaConsumerConfig {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, usersGroupId);
-        props.put(ConsumerConfig.CLIENT_ID_CONFIG, usersClientId);
+        props.put(ConsumerConfig.CLIENT_ID_CONFIG, clientId);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffsetReset);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
