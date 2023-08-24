d7477aaa7a531d6a312c7156957c84416c40f715
bgalgamu <kevin.si1396@gmail.com>
2023-08-24 17:07:05 +0200

    put json property for correct property name

--- connector/rest/src/main/java/it/pagopa/selfcare/external_interceptor/connector/rest/model/EncodedParamForm.java
+++ connector/rest/src/main/java/it/pagopa/selfcare/external_interceptor/connector/rest/model/EncodedParamForm.java
@@ -1,12 +1,16 @@
 package it.pagopa.selfcare.external_interceptor.connector.rest.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class EncodedParamForm {
+    @JsonProperty("grant_type")
     private String grantType;
+    @JsonProperty("client_id")
     private String clientId;
+    @JsonProperty("client_secret")
     private String clientSecret;
 }
