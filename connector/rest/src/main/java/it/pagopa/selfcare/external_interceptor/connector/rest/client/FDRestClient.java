d0e319ac24555dcf51d822757f694b2d7c6cad76
bgalgamu <kevin.si1396@gmail.com>
2023-08-24 15:11:48 +0200

    fix feign client encoder issues

--- connector/rest/src/main/java/it/pagopa/selfcare/external_interceptor/connector/rest/client/FDRestClient.java
+++ connector/rest/src/main/java/it/pagopa/selfcare/external_interceptor/connector/rest/client/FDRestClient.java
@@ -1,19 +1,13 @@
 package it.pagopa.selfcare.external_interceptor.connector.rest.client;
 
-import feign.codec.Encoder;
-import feign.form.spring.SpringFormEncoder;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "${rest-client.prod-fd.serviceCode}",  url = "${rest-client.prod-fd.base-url}")
 public interface FDRestClient {
-    @Bean
-    default Encoder feignFormEncoder() {
-        return new SpringFormEncoder();
-    }
-    @PostMapping(value = "${rest-client.prod-fd.login.path}")
+    @PostMapping(value = "${rest-client.prod-fd.login.path}", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     String getFDToken(@RequestParam("grant_type") String grantType,
                       @RequestParam("client_secret") String clientSecret,
                       @RequestParam("client_id") String clientId);
