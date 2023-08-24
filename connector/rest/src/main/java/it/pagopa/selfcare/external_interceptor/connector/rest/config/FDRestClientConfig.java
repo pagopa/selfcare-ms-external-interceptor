72a60d8391e80825c50b98ab2a698fe2ff5b323e
bgalgamu <kevin.si1396@gmail.com>
2023-08-24 12:30:41 +0200

    fix feign client config

--- connector/rest/src/main/java/it/pagopa/selfcare/external_interceptor/connector/rest/config/FDRestClientConfig.java
+++ connector/rest/src/main/java/it/pagopa/selfcare/external_interceptor/connector/rest/config/FDRestClientConfig.java
@@ -3,6 +3,7 @@ package it.pagopa.selfcare.external_interceptor.connector.rest.config;
 import feign.codec.Encoder;
 import feign.form.spring.SpringFormEncoder;
 import it.pagopa.selfcare.commons.connector.rest.config.RestClientBaseConfig;
+import it.pagopa.selfcare.external_interceptor.connector.rest.client.FDRestClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +13,7 @@ import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @Import(RestClientBaseConfig.class)
-@EnableFeignClients(clients = FDRestClientConfig.class)
+@EnableFeignClients(clients = FDRestClient.class)
 @PropertySource("classpath:config/prod-fd-rest-client.properties")
 public class FDRestClientConfig {
     @Bean
