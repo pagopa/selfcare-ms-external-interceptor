8a80f2e7a1d17004b657d9d59b55359721d486ba
bgalgamu <kevin.si1396@gmail.com>
2023-08-24 14:15:04 +0200

    fix feign client encoder issues

--- connector/rest/src/main/java/it/pagopa/selfcare/external_interceptor/connector/rest/client/FDRestClient.java
+++ connector/rest/src/main/java/it/pagopa/selfcare/external_interceptor/connector/rest/client/FDRestClient.java
@@ -1,15 +1,23 @@
 package it.pagopa.selfcare.external_interceptor.connector.rest.client;
 
-import feign.Headers;
+import feign.codec.Encoder;
+import feign.form.spring.SpringFormEncoder;
+import it.pagopa.selfcare.external_interceptor.connector.rest.config.FDRestClientConfig;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "${rest-client.prod-fd.serviceCode}", url = "${rest-client.prod-fd.base-url}")
+@Qualifier("prod-fd")
+@FeignClient(name = "${rest-client.prod-fd.serviceCode}", configuration = FDRestClientConfig.class, url = "${rest-client.prod-fd.base-url}")
 public interface FDRestClient {
-
-    @PostMapping(value = "${rest-client.prod-fd.login.path}")
-    @Headers("Content-Type: application/x-www-form-urlencoded")
+    @Bean
+    default Encoder feignFormEncoder() {
+        return new SpringFormEncoder();
+    }
+    @PostMapping(value = "${rest-client.prod-fd.login.path}", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     String getFDToken(@RequestParam("grant_type") String grantType,
                       @RequestParam("client_secret") String clientSecret,
                       @RequestParam("client_id") String clientId);
--- connector/rest/src/main/java/it/pagopa/selfcare/external_interceptor/connector/rest/config/FDRestClientConfig.java
+++ connector/rest/src/main/java/it/pagopa/selfcare/external_interceptor/connector/rest/config/FDRestClientConfig.java
@@ -1,11 +1,8 @@
 package it.pagopa.selfcare.external_interceptor.connector.rest.config;
 
-import feign.codec.Encoder;
-import feign.form.spring.SpringFormEncoder;
 import it.pagopa.selfcare.commons.connector.rest.config.RestClientBaseConfig;
 import it.pagopa.selfcare.external_interceptor.connector.rest.client.FDRestClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
@@ -16,8 +13,5 @@ import org.springframework.context.annotation.PropertySource;
 @EnableFeignClients(clients = FDRestClient.class)
 @PropertySource("classpath:config/prod-fd-rest-client.properties")
 public class FDRestClientConfig {
-    @Bean
-    public Encoder feignFormEncoder() {
-        return new SpringFormEncoder();
-    }
+
 }
