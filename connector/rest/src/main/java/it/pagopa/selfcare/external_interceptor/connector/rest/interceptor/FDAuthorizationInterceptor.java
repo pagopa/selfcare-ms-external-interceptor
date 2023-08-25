179e646a21d032b90b896be346a332fa4689105e
bgalgamu <kevin.si1396@gmail.com>
2023-08-25 18:52:25 +0200

    remove authorization header

--- connector/rest/src/main/java/it/pagopa/selfcare/external_interceptor/connector/rest/interceptor/FDAuthorizationInterceptor.java
+++ connector/rest/src/main/java/it/pagopa/selfcare/external_interceptor/connector/rest/interceptor/FDAuthorizationInterceptor.java
@@ -3,6 +3,7 @@ package it.pagopa.selfcare.external_interceptor.connector.rest.interceptor;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -11,6 +12,7 @@ public class FDAuthorizationInterceptor implements RequestInterceptor {
     @Override
     public void apply(RequestTemplate template) {
             template.header("Ocp-Apim-Subscription-Key", (String) null)
-                    .header("x-selfcare-uid", (String) null);
+                    .header("x-selfcare-uid", (String) null)
+                    .header(HttpHeaders.AUTHORIZATION, (String) null);
     }
 }
