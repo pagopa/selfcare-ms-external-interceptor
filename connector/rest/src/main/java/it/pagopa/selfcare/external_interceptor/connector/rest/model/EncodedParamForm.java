3ce16e785bc712f670173a7ec1c0ca11aedd1abf
bgalgamu <kevin.si1396@gmail.com>
2023-08-25 14:13:15 +0200

    add test to new condition on ec institution notifications and make EncodedParamForm properties snake_case

--- connector/rest/src/main/java/it/pagopa/selfcare/external_interceptor/connector/rest/model/EncodedParamForm.java
+++ connector/rest/src/main/java/it/pagopa/selfcare/external_interceptor/connector/rest/model/EncodedParamForm.java
@@ -1,16 +1,10 @@
 package it.pagopa.selfcare.external_interceptor.connector.rest.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public class EncodedParamForm {
-    @JsonProperty("grant_type")
-    private String grantType;
-
-    @JsonProperty("client_id")
-    private String clientId;
-
-    @JsonProperty("client_secret")
-    private String clientSecret;
+    private String grant_type;
+    private String client_id;
+    private String client_secret;
 }
