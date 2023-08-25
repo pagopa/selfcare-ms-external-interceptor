d7b41595393bfcfcd2280eb56316cd0905eb9e1a
bgalgamu <kevin.si1396@gmail.com>
2023-08-25 18:59:36 +0200

    remove authorization header

--- connector/rest/src/main/java/it/pagopa/selfcare/external_interceptor/connector/rest/client/FDRestClient.java
+++ connector/rest/src/main/java/it/pagopa/selfcare/external_interceptor/connector/rest/client/FDRestClient.java
@@ -2,6 +2,7 @@ package it.pagopa.selfcare.external_interceptor.connector.rest.client;
 
 import feign.Headers;
 import feign.Param;
+import it.pagopa.selfcare.external_interceptor.connector.rest.interceptor.FDAuthorizationInterceptor;
 import it.pagopa.selfcare.external_interceptor.connector.rest.model.EncodedParamForm;
 import it.pagopa.selfcare.external_interceptor.connector.rest.model.auth.OauthToken;
 import it.pagopa.selfcare.external_interceptor.connector.rest.model.prod_fd.OrganizationLightBeanResponse;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "${rest-client.prod-fd.serviceCode}", url = "${rest-client.prod-fd.base-url}")
+@FeignClient(name = "${rest-client.prod-fd.serviceCode}", url = "${rest-client.prod-fd.base-url}", configuration = FDAuthorizationInterceptor.class)
 public interface FDRestClient {
     @PostMapping(value = "${rest-client.prod-fd.login.path}", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     OauthToken getFDToken(@RequestBody EncodedParamForm encodedParamForm);
