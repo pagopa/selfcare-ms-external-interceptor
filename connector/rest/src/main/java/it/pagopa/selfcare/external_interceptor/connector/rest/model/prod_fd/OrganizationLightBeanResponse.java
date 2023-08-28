d25405b1ac3ee37dbdd5e07fd5d3a23f52c4348c
bgalgamu <kevin.si1396@gmail.com>
2023-08-28 16:41:56 +0200

    remove all argConstructor

--- connector/rest/src/main/java/it/pagopa/selfcare/external_interceptor/connector/rest/model/prod_fd/OrganizationLightBeanResponse.java
+++ connector/rest/src/main/java/it/pagopa/selfcare/external_interceptor/connector/rest/model/prod_fd/OrganizationLightBeanResponse.java
@@ -1,10 +1,8 @@
 package it.pagopa.selfcare.external_interceptor.connector.rest.model.prod_fd;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class OrganizationLightBeanResponse {
     private boolean alreadyRegistered;
     private OrganizationResponse organization;
