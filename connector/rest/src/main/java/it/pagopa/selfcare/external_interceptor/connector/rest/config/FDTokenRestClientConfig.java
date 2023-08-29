f10a5c12437aef060d9ef06d82ff701885cd4332
bgalgamu <kevin.si1396@gmail.com>
2023-08-29 18:45:49 +0200

    add restClient base Config from commons

--- connector/rest/src/main/java/it/pagopa/selfcare/external_interceptor/connector/rest/config/FDTokenRestClientConfig.java
+++ connector/rest/src/main/java/it/pagopa/selfcare/external_interceptor/connector/rest/config/FDTokenRestClientConfig.java
@@ -1,19 +1,15 @@
 package it.pagopa.selfcare.external_interceptor.connector.rest.config;
 
-import feign.RequestInterceptor;
-import it.pagopa.selfcare.commons.connector.rest.interceptor.QueryParamsPlusEncoderInterceptor;
+import it.pagopa.selfcare.commons.connector.rest.config.RestClientBaseConfig;
 import it.pagopa.selfcare.external_interceptor.connector.rest.client.FDTokenRestClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@Import(RestClientBaseConfig.class)
 @EnableFeignClients(clients = FDTokenRestClient.class)
 @PropertySource("classpath:config/token-fd-rest-client.properties")
 public class FDTokenRestClientConfig {
-    @Bean
-    public RequestInterceptor queryParamsPlusEncoderInterceptor() {
-        return new QueryParamsPlusEncoderInterceptor();
-    }
 }
