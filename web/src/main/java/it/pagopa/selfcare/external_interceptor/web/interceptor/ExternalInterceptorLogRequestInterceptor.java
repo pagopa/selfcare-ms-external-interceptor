221e0d5ff133116249cc5005e75cecf79b4f2239
bgalgamu <kevin.si1396@gmail.com>
2023-09-11 17:40:14 +0200

    fix test

--- web/src/main/java/it/pagopa/selfcare/external_interceptor/web/interceptor/ExternalInterceptorLogRequestInterceptor.java
+++ web/src/main/java/it/pagopa/selfcare/external_interceptor/web/interceptor/ExternalInterceptorLogRequestInterceptor.java
@@ -1,6 +1,15 @@
 package it.pagopa.selfcare.external_interceptor.web.interceptor;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -8,6 +17,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Collection;
 import java.util.List;
+import java.util.TimeZone;
+
 @Slf4j
 @Component
 public class ExternalInterceptorLogRequestInterceptor implements HandlerInterceptor {
@@ -25,5 +36,20 @@ public class ExternalInterceptorLogRequestInterceptor implements HandlerIntercep
             log.info("Requested {} {}, server Id: {}, controller: {}", request.getMethod(), request.getRequestURI(), request.getServerName(), controller.toString());
         }
         return true;
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.registerModule(new Jdk8Module());
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE);
+        mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        mapper.setTimeZone(TimeZone.getDefault());
+        return mapper;
     }
 }
