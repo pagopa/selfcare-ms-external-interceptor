f6942d7a1e972142113fbb3b498753c5ab2b8f73
Kevin Silva <84286727+KevinSi96@users.noreply.github.com>
2023-09-12 09:05:08 +0200

    remove custom log request interceptor (#28)

--- web/src/main/java/it/pagopa/selfcare/external_interceptor/web/config/WebConfig.java
+++ web/src/main/java/it/pagopa/selfcare/external_interceptor/web/config/WebConfig.java
@@ -1,29 +1,11 @@
 package it.pagopa.selfcare.external_interceptor.web.config;
 
-import org.springframework.context.annotation.ComponentScan;
+import it.pagopa.selfcare.commons.web.config.BaseWebConfig;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Collection;
+import org.springframework.context.annotation.Import;
 
 @Configuration
-@ComponentScan(basePackages = {"it.pagopa.selfcare.external_interceptor.web.interceptor"})
-class WebConfig implements WebMvcConfigurer {
-    private final Collection<HandlerInterceptor> interceptors;
-
-
-    public WebConfig(Collection<HandlerInterceptor> interceptors) {
-        this.interceptors = interceptors;
-    }
-
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        if (interceptors != null) {
-            interceptors.forEach(registry::addInterceptor);
-        }
-    }
+@Import(BaseWebConfig.class)
+class WebConfig{
 
 }
