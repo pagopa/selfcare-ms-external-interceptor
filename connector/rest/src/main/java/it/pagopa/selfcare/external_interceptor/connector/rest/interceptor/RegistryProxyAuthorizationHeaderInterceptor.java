ea97d1461b6365c1dc929ca5341bdd7418773767
Kevin Silva <kevin.si1396@gmail.com>
2024-03-08 12:08:10 +0100

    remove service annotation from RegistryProxyAuthorizationHeaderInterceptor

--- connector/rest/src/main/java/it/pagopa/selfcare/external_interceptor/connector/rest/interceptor/RegistryProxyAuthorizationHeaderInterceptor.java
+++ connector/rest/src/main/java/it/pagopa/selfcare/external_interceptor/connector/rest/interceptor/RegistryProxyAuthorizationHeaderInterceptor.java
@@ -6,7 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 
-//@Service
 @Slf4j
 public class RegistryProxyAuthorizationHeaderInterceptor implements RequestInterceptor {
 
